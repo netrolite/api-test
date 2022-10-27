a02f4ac9ec3aaee2cb9591a3a328846c4d5c8f4f
matvey733 <morozovmatvej75@gmail.com>
2022-10-27 10:39:55 +0300

    API request added (not working)

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 import React, { useState } from 'react';
+
+export default function App() {
+
+        let api = fetch("https://reqres.in/api/users")
+        .then(res => res.json)
+        .then(res => console.log(res))
+    
+    
+    return (
+        <main>
+
+        </main>
+    )
+}
